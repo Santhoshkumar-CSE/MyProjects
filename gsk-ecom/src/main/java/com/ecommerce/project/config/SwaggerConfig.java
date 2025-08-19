dbdc7ddad5b732ec07f34efd78a5e1fb8104822c
Santhoshkumar.G <santhoshkumar.g2020@vitstudent.ac.in>
2025-08-19 20:58:13 +0530

    ecommerce project

---
+++ gsk-ecom/src/main/java/com/ecommerce/project/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.ecommerce.project.config;
+
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI()
+    {
+        SecurityScheme bearerScheme=new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("JWT Bearer Token");
+
+        SecurityRequirement bearerRequirement=new SecurityRequirement()
+                .addList("Bearer Authentication");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("GSK ecom API").version("1.0")
+                        .description("This is Spring Boot application for eCommerce")
+                        .license(new License().name("Apache 2.0").
+                                url("http://gsk.com"))
+                        .contact(new Contact().name("SanthoshKumar").email("gskecom@gmail.com"))
+                ).externalDocs(new ExternalDocumentation()
+                        .description("Project Documentation")
+                        .url("http://gsk-ecom"))
+                .components(new Components()
+                .addSecuritySchemes("Bearer Authentication",bearerScheme))
+                .addSecurityItem(bearerRequirement);
+
+    }
+}
